3eca8bcf8c9eef1464095c3728056464cd392d4f
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-07 14:56:32 -0600

    sending message from client

--- src/main/java/com/example/websocket_chat/client/ClientGUI.java
+++ src/main/java/com/example/websocket_chat/client/ClientGUI.java
@@ -1,9 +1,12 @@
 package com.example.websocket_chat.client;
 
+import com.example.websocket_chat.Message;
+
 import java.util.concurrent.ExecutionException;
 
 public class ClientGUI {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         MyStompClient myStompClient = new MyStompClient("user1");
+        myStompClient.sendMessage(new Message("user1", "Hello World!"));
     }
 }
