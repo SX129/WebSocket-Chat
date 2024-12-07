ce653378bd788dda0308d47141189342e22085a0
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-07 13:50:52 -0600

    implemented class to handle new connected users

---
+++ src/main/java/com/example/websocket_chat/client/MyStompSessionHandler.java
@@ -0,0 +1,47 @@
+package com.example.websocket_chat.client;
+
+import com.example.websocket_chat.Message;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+
+import java.lang.reflect.Type;
+
+public class MyStompSessionHandler extends StompSessionHandlerAdapter {
+    private String username;
+
+    public MyStompSessionHandler(String username){
+        this.username = username;
+    }
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders){
+        // automatically subscribe users to /topic/messages
+        session.subscribe("topic/messages", new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return Message.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                try{
+                    if(payload instanceof Message){
+                        Message message = (Message) payload;
+                        System.out.println("Received message: " + message.getUser() + ": " + message.getMessage());
+                    }else{
+                        System.out.println("Received unexpected payload type: " + payload.getClass());
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception){
+        exception.printStackTrace();
+    }
+}
