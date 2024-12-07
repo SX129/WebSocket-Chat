d9988c01e3bd6172bf3f73e455afd50df3b90343
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-07 13:54:14 -0600

    added session connectivity

--- src/main/java/com/example/websocket_chat/client/MyStompClient.java
+++ src/main/java/com/example/websocket_chat/client/MyStompClient.java
@@ -2,6 +2,7 @@ package com.example.websocket_chat.client;
 
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
@@ -10,13 +11,14 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MyStompClient {
     // allows us to connect to stomp servers (what the websocket server is based on)
     private StompSession session;
     private String username;
 
-    public MyStompClient(String username){
+    public MyStompClient(String username) throws ExecutionException, InterruptedException {
         this.username = username;
 
         // create a list of transports (protocols) to use
@@ -29,5 +31,12 @@ public class MyStompClient {
 
         // allows stompClient to automatically serialize and deserialize Java objects and JSON for the websocket
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+
+        // session handler to handle the new user
+        StompSessionHandler sessionHandler = new MyStompSessionHandler(username);
+
+        //URL for websocket uses ws://
+        String url = "ws://localhost:8080/ws";
+        session = stompClient.connectAsync(url, sessionHandler).get();
     }
 }
