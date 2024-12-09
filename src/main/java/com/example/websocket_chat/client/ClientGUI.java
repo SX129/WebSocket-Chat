88e05d09bfc3bb571637cb28c3a4078331de36ee
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-08 18:41:20 -0600

    added functionality to client gui

--- src/main/java/com/example/websocket_chat/client/ClientGUI.java
+++ src/main/java/com/example/websocket_chat/client/ClientGUI.java
@@ -2,11 +2,30 @@ package com.example.websocket_chat.client;
 
 import com.example.websocket_chat.Message;
 
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.concurrent.ExecutionException;
 
-public class ClientGUI {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        MyStompClient myStompClient = new MyStompClient("user1");
-        myStompClient.sendMessage(new Message("user1", "Hello World!"));
+public class ClientGUI extends JFrame {
+    public ClientGUI(String username){
+        super("User: " + username);
+
+        setSize(1218, 685);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int option = JOptionPane.showConfirmDialog(ClientGUI.this,
+                        "Are you sure you want to exit?",
+                        "Exit",
+                        JOptionPane.YES_NO_OPTION);
+
+                if (option == JOptionPane.YES_OPTION) {
+                    ClientGUI.this.dispose();
+                }
+            }
+        });
     }
 }
