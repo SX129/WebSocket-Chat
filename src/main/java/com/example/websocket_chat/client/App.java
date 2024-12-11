ace969c6b557f59127fe0b19b6fd138a13457581
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-10 19:16:45 -0600

    added try catch

--- src/main/java/com/example/websocket_chat/client/App.java
+++ src/main/java/com/example/websocket_chat/client/App.java
@@ -1,13 +1,21 @@
 package com.example.websocket_chat.client;
 
 import javax.swing.*;
+import java.util.concurrent.ExecutionException;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                ClientGUI clientGUI = new ClientGUI("user1");
+                ClientGUI clientGUI = null;
+                try {
+                    clientGUI = new ClientGUI("user1");
+                } catch (ExecutionException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
 
                 clientGUI.setVisible(true);
             }
