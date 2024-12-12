fc70ca31d8810e5e28be54c3f3c8ad1ee60059fd
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-11 19:08:20 -0600

    added username input field

--- src/main/java/com/example/websocket_chat/client/App.java
+++ src/main/java/com/example/websocket_chat/client/App.java
@@ -8,9 +8,22 @@ public class App {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                String username = JOptionPane.showInputDialog(null,
+                        "Enter Username (Max: 16 Characters): ",
+                        "Chat Application",
+                        JOptionPane.QUESTION_MESSAGE);
+
+                if (username == null || username.isEmpty() || username.length() > 16) {
+                    JOptionPane.showMessageDialog(null,
+                            "Invalid Username",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
                 ClientGUI clientGUI = null;
                 try {
-                    clientGUI = new ClientGUI("user1");
+                    clientGUI = new ClientGUI(username);
                 } catch (ExecutionException e) {
                     throw new RuntimeException(e);
                 } catch (InterruptedException e) {
