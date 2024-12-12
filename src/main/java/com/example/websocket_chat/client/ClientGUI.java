59bfec202a01e54861237f66dc492cb678179770
Henry Nguyen <nguyenhenry682@gmail.com>
2024-12-11 19:12:59 -0600

    added scrolling to message panel

--- src/main/java/com/example/websocket_chat/client/ClientGUI.java
+++ src/main/java/com/example/websocket_chat/client/ClientGUI.java
@@ -3,6 +3,8 @@ package com.example.websocket_chat.client;
 import com.example.websocket_chat.Message;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -15,6 +17,7 @@ public class ClientGUI extends JFrame implements MessageListener{
     private JPanel connectedUsersPanel, messagePanel;
     private MyStompClient myStompClient;
     private String username;
+    private JScrollPane messagePanelScrollPane;
 
     public ClientGUI(String username) throws ExecutionException, InterruptedException {
         super("User: " + username);
@@ -72,7 +75,20 @@ public class ClientGUI extends JFrame implements MessageListener{
         messagePanel = new JPanel();
         messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.Y_AXIS));
         messagePanel.setBackground(Utilities.TRANSPARENT_COLOR);
-        chatPanel.add(messagePanel, BorderLayout.CENTER);
+
+        messagePanelScrollPane = new JScrollPane(messagePanel);
+        messagePanelScrollPane.setBackground(Utilities.TRANSPARENT_COLOR);
+        messagePanelScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        messagePanelScrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        messagePanelScrollPane.getViewport().addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                revalidate();
+                repaint();
+            }
+        });
+
+        chatPanel.add(messagePanelScrollPane, BorderLayout.CENTER);
 
         JPanel inputPanel = new JPanel();
         inputPanel.setBorder(Utilities.addPadding(10, 10, 10, 10));
